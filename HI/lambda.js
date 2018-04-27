c130bfbed48af4a09ff11083a714d473c322f48a
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-04-27 17:03:25 +0530

    commit

--- HI/lambda.js
+++ HI/lambda.js
@@ -5,7 +5,7 @@ exports.handler = function (event, context, callback) {
 	console.error(event);
 	ddb.get({
 		TableName: 'jkjk',
-		Key: { 'jkjk': 'kls ' }
+		Key: { 'jkjk': 'klss yes' }
 	}, function (err, data) {
 		if (err) {
 			//handle error
