d3763dd03bd26867a7956aa0582b8d0b4249811e
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-05-01 17:03:57 +0530

    kjk

--- Hi/lambda.js
+++ Hi/lambda.js
@@ -1,7 +1,18 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	ddb.get({
+		TableName: 'Calculator',
+		Key: { 'ID': kl, 'Result': kl }
+	}, function (err, data) {
+		if (err) {
+			//handle error
+		} else {
+			//your logic goes here
+		}
+	});
 
 	console.log("Log from other region");
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
